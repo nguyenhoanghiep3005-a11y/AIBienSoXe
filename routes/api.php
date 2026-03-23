2253e677c181b661571ad265fd0ea2fd4d990956
HoangHiep <nguyenhoanghiep3005@gmail.com>
2026-03-23 16:38:05 +0700

    updatecode

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('lpr')->group(function () {
+    Route::post('/detect', [LprApiController::class, 'detect']);
+    Route::post('/save', [LprApiController::class, 'save']);
+    Route::get('/list', [LprApiController::class, 'list']);
 });
